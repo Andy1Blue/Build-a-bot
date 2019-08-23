285aa7281f8e74f2301dcacb8674fda7194ab3bd
Krzysztof Bujak <bujak.krzysztof@outlook.com>
2019-08-23 17:38:47 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'Home'}" exact>
+              <img src="./assets/build-a-bot-logo.png" alt class="logo" />
+              Build-a-Bot
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'Build'}" exact>Build</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'BrowseParts'}" exact>Parts</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <div class="container">
+      <aside class="aside">
+        <router-view name="sidebar" />
+      </aside>
+      <main>
+        <router-view />
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-  },
+  name: "app"
 };
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+
+main {
+  padding: 30px;
+  background-color: #fff;
+  width: 964px;
+  min-height: 300px;
+}
+
+header {
+  background-color: #999;
+  width: 1184px;
+  margin: 0 auto;
+}
+
+ul {
+  padding: 3px;
+  display: flex;
+}
+
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+
+.router-link-active {
+  color: #fff;
+}
+
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+
+.aside {
+  padding: 30px;
+  background-color: #aaa;
+  width: 100px;
+  min-height: 300px;
 }
 </style>
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.user(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    cart: [],
+  },
+  mutation: {
+    addRobotToCart(state, robot) {
+      state.cart.push(robot);
+    },
+  },
+});
